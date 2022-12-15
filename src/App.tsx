aa2fe5f28afd99fec942dc59aabdaa6f7f9a86ef
ro-nin <ema.cio1793@gmail.com>
2022-12-15 15:52:42 +0100

    basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card';
+import CardDetailsForm from './Components/CardDetailsForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='content'>
+        <div className="gradientContainer"></div>
+        <div className="formContainer">
+          <CardDetailsForm></CardDetailsForm>
+          {/*success && thank you! ? */}
+          <button className='confirmButton'>Confirm</button>
+        </div>
+        <div className='cardPreviewContainer'>
+          <Card></Card>
+        </div>
+      </div>
     </div>
   );
 }
