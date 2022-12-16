594cdcdfbb6f2fe8e3357ccdc76eff6379d735a3
ro-nin <ema.cio1793@gmail.com>
2022-12-16 16:40:55 +0100

    card layout

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -21,17 +21,17 @@ const Card = ({ data, front }: Props) => {
           <div className="topRow" >
             <img src={Logo} alt='card logo' />
           </div>
-          <div className="bottomRow">
-            <div className="" >
-              <span>{cardNumber?.slice(0,4)} </span>
-              <span>{cardNumber?.slice(4,8)} </span>
-              <span>{cardNumber?.slice(8,12)} </span>
-              <span>{cardNumber?.slice(12,16)}</span>
+          <div className="bottomRow" style={{ padding: '3rem 3rem 4rem 3rem' }}>
+            <div className="" style={{ textAlign: "left", paddingBottom: '2rem', fontSize: '1.5rem' }} >
+              <span>{cardNumber?.slice(0, 4)} </span>
+              <span>{cardNumber?.slice(4, 8)} </span>
+              <span>{cardNumber?.slice(8, 12)} </span>
+              <span>{cardNumber?.slice(12, 16)}</span>
             </div>
-            <div className="" style={{ display: "flex", justifyContent: 'space-between', padding: ' 0 3rem 2rem' }}>
+            <div className="" style={{ display: "flex", justifyContent: 'space-between', }}>
               <span>{cardHolderName}</span>
               <div className="">
-                <span>{expMM}</span>
+                <span>{expMM?.length === 1 ? `0${expMM}` : expMM}</span>
                 <span>/</span>
                 <span>{expYY}</span>
               </div>
