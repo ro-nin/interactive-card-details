7ffcba9775d511a0ad80a29bc47ec35a53004e53
ro-nin <ema.cio1793@gmail.com>
2022-12-16 20:23:06 +0100

    more tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -26,3 +26,48 @@ test('Bad Card Format', async () => {
         expect(errorLabel).toBeInTheDocument()
     })
 })
+
+test('No Year Specified', async () => {
+    render(<App />);
+    const yearInput = screen.getByPlaceholderText('YY');
+    expect(yearInput).toBeInTheDocument()
+    userEvent.click(screen.getByRole('button'));
+    await waitFor(() => {
+        const errorLabel = screen.getByText(/Insert a year/i);
+        expect(errorLabel).toBeInTheDocument()
+    })
+
+})
+
+
+test('Create Correctly', async () => {
+    render(<App />);
+    const cardInput = screen.getByLabelText('CARD NUMBER');
+    fireEvent.change(cardInput, { target: { value: '9591648963891012' } })
+    expect(screen.getByText('9591')).toBeInTheDocument()
+    expect(screen.getByText('6489')).toBeInTheDocument()
+    expect(screen.getByText('6389')).toBeInTheDocument()
+    expect(screen.getByText('1012')).toBeInTheDocument()
+
+    const nameInput = screen.getByPlaceholderText('e.g. Jane Appleseed');
+    fireEvent.change(nameInput, { target: { value: 'Test McTesty' } })
+
+    const monthInput = screen.getByPlaceholderText('MM');
+    fireEvent.change(monthInput, { target: { value: '11' } })
+    const yearInput = screen.getByPlaceholderText('YY');
+    fireEvent.change(yearInput, { target: { value: '25' } })
+
+    const cvcInput = screen.getByPlaceholderText('e.g. 123');
+    fireEvent.change(cvcInput, { target: { value: '123' } })
+
+    userEvent.click(screen.getByRole('button'));
+    await waitFor(() => {
+        const thanklabel = screen.getByText(/THANK YOU/i);
+        expect(thanklabel).toBeInTheDocument()
+    })
+    userEvent.click(screen.getByRole('button'));
+    await waitFor(() => {
+        expect(screen.getByPlaceholderText('e.g. Jane Appleseed')).toBeInTheDocument();
+    })
+
+}) 
