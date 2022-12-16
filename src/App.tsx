44122e989e1a2ee97cf9402875bc4f533058b9a0
ro-nin <ema.cio1793@gmail.com>
2022-12-16 12:08:20 +0100

    avoid layout shift on error

--- src/App.tsx
+++ src/App.tsx
@@ -35,27 +35,33 @@ function App() {
           <form onSubmit={handleSubmit(onSubmit)}>
             <label>CARDHOLDER NAME</label>
             <input className='inputLarge' defaultValue="" {...register("cardHolderName", { required: true })} />
-            {errors.cardHolderName && <span>Error: name</span>}
-           
+            <span className='errorLabel'>{errors.cardHolderName ? "Error: cardHolderName" : ''}</span>
+
             <label>CARD NUMBER</label>
             <input className='inputLarge' {...register("cardNumber", { required: true })} />
-            {errors.cardNumber && <span>Error: cardNumber</span>}
-            
-            <div className="" style={{ width: '100%', display: "flex", gap: "2rem", alignItems: "start" }}>
-              <div className="" style={{ alignItems: "start", textAlign: 'left' }}>
+            <span className='errorLabel'>{errors.cardNumber ? "Error: cardNumber" : ''}</span>
+
+            <div className="" style={{ width: '100%', display: "flex", alignItems: "start", gap: '2rem' }}>
+              <div className="" style={{ flexGrow: '1', flexShrink: '0', textAlign: 'left' }}>
+
                 <label>EXP. DATE (MM/YY)</label>
-                <div className="" style={{ display: "flex", gap: ".4rem", alignItems: "start" }}>
-                  <input className='inputSmall' {...register("expMM", { required: true, maxLength: 2, min: 1, max: 12 })} />
-                  {errors.expMM && <span>exm month</span>}
-                  <input className='inputSmall' {...register("expYY", { required: true, maxLength: 2, min: 1, max: 99 })} />
-                  {errors.expYY && <span>exp year</span>}
+                <div className="" style={{ display: "flex", alignItems: "start", flexDirection: 'column', flexShrink: 0 }}>
+                  <div className="" style={{ display: "flex", gap: '.5rem' }}>
+                    <input className='inputSmall' {...register("expMM", { required: true, maxLength: 2, min: 1, max: 12 })} />
+                    <input className='inputSmall' {...register("expYY", { required: true, maxLength: 2, min: 1, max: 99 })} />
+                  </div>
+                  <div className="">
+                    {errors.expMM ? <span className='errorLabel' >exm month</span> : <span style={{ display: 'block', }}> </span>}
+                    {errors.expYY ? <span className='errorLabel' >exp year</span> : <span style={{ display: 'block' }}> </span>}
+                  </div>
+
                 </div>
               </div>
-              <div className="" style={{ flexGrow: '1', flexShrink: '0', alignItems: "start", alignSelf: "end", textAlign: 'left' }}>
+              <div className="" style={{ flexGrow: '1', textAlign: 'left' }}>
                 <label>CVC</label>
-                <div className="" style={{ display: 'flex', }}>
-                  <input className='' style={{ flexGrow: '1', flexShrink: '0', }} type={"number"} {...register("cvc", { required: true, maxLength: 3, min: 1, max: 999 })} />
-                  {errors.cvc && <span>CVC</span>}
+                <div className="" >
+                  <input className=' ' style={{ width: '100%', }} type={"number"} {...register("cvc", { required: true, maxLength: 3, min: 1, max: 999 })} />
+                  <span className='errorLabel'>{errors.cvc ? "CVC" : ''}</span>
                 </div>
               </div>
 
